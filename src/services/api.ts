1f5dfc9acc275dee27a91731dfe3e1014acf73d2
felixelement <felix@element6.io>
2023-12-16 11:39:24 -0500

    test build

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,6 @@ interface ApiProp {
   url: string;
   params?: any;
 }
-
 const apiURL: string | undefined = process.env.NEXT_PUBLIC_API_URL;
 const apiToken: string | undefined = process.env.API_TOKEN;
 
